8393699768556793f7a8c2bb1ebadeb7844dfb62
TySchultz <Tyschultz7@gmail.com>
2016-08-17 21:47:47 -0400

    added tableview and print out of home and away teams

--- TestNFLGames/Downloader.swift
+++ TestNFLGames/Downloader.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Kanna
 
-struct Event {
+struct Game {
     var homeTeam: String
     var awayTeam: String
     var date: String
@@ -18,10 +18,8 @@ struct Event {
 }
 
 class Downloader: NSObject {
-    var events : [[Event]]!
-    var buildings : [String]!
-    
-    func downloadSchedule() -> ([[Event]]!,[String]!, Int) {
+    var games :[Game] = []
+    func downloadSchedule() -> ([Game]) {
         
         var status = 0
         // Set the page URL we want to download
@@ -37,13 +35,13 @@ class Downloader: NSObject {
                 let content = doc.css("g")
                 for row in content {
                     
-                    let ev = Event(homeTeam: row.xpath("@hnn").first!.text!,
+                    let ev = Game(homeTeam: row.xpath("@hnn").first!.text!,
                                    awayTeam: row.xpath("@vnn").first!.text!,
                                    date: row.xpath("@eid").first!.text!,
                                    homeScore: row.xpath("@hs").first!.text!,
                                    awayScore: row.xpath("@vs").first!.text!)
                     
-                    
+                    games.append(ev)
                     //<g eid="2016082057" gsis="56859" d="Sat" t="9:00" q="P" h="LA" hnn="rams" hs="0" v="KC" vnn="chiefs" vs="0" rz="0" ga="" gt="PRE"/>
                     print ("Vote on the \(ev)")
                                   }
@@ -53,6 +51,6 @@ class Downloader: NSObject {
             status = error.code
         }
 
-        return (events, buildings,status)
+        return (games)
     }
  }
