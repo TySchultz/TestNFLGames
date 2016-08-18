9ce4399d0fe382e3abb0e79f67b39d5373ea7a16
narmold <narmold@gmail.com>
2016-08-18 00:25:26 -0400

    Messed around trying to display pictures. Was going to implement database but second guessed our Firebase choice

---
+++ TestNFLGames/Assets.xcassets/NFL_Shields-master/README.dataset/README.md
@@ -0,0 +1,4 @@
+# NFL_Shields
+Awesome team shields created for PickUp
+
+![Alt text](shields.png?raw=true "shields.png")
---
+++ TestNFLGames/GameTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GameTableViewCell.swift
+//  TestNFLGames
+//
+//  Created by Nick Armold on 8/17/16.
+//  Copyright © 2016 Ty Schultz. All rights reserved.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
